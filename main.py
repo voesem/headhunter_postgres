9b0dc008121bda8699639d3dde7ab4eedc8fa50f
Vladimir Semikov <voesem@ya.ru>
2023-08-24 20:41:19 +0300

    реализована основная логика работы программы

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from utils import get_employer_id_from_json_file, HeadHunterAPI
+from utils import get_employer_id_from_json_file, HeadHunterAPI, create_database, save_data_to_database, DBManager
+
+from config import config
 
 
 def main():
@@ -6,16 +8,18 @@ def main():
     filename = 'employers.json'
     ids = get_employer_id_from_json_file(filename)
     data = []
+    database_name = 'headhunter'
+    params = config()
     for i in ids:
         employer_name = hh.get_employer_info(i)['name']
-        vacancies_by_employer = hh.get_vacancies_from_employer(i)
 
         data.append({
             'Компания': employer_name,
-            'Вакансии': vacancies_by_employer
+            'Вакансии': hh.get_vacancies_from_employer(i)
         })
 
-    print(data)
+    create_database(database_name, params)
+    save_data_to_database(data, database_name, params)
 
 
 if __name__ == '__main__':
